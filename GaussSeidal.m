a4d52ef8b1caff75c34a60ec7c701c4f16467186
MuhammadFaisalm <mfaisal@aucegypt.edu>
2018-10-18 01:07:37 +0200

    Added relaxation feature and tracking both Ea and iteration number for answers

--- GaussSeidal.m
+++ GaussSeidal.m
@@ -1,22 +1,27 @@
-function [ X ] = GaussSeidal( XInitial, coefficients, Bs, MaxIterNum, Ea )
+function [ X, it, EA ] = GaussSeidal( XInitial, coefficients, Bs, MaxIterNum, Ea, relax)
 
 %GAUSSSEIDAL General Implementation of the Gauss-Seidal Calculation
 
 Augmented  = [XInitial; XInitial];
 [paramsNum, LiterallyOne] = size(XInitial);
 X = "Ea Error";
+it = MaxIterNum;
+EA = 100;
 
 for iter = 1 : MaxIterNum
 
     for i = 1 : paramsNum
-        Augmented(i) = sum(coefficients(i,:).*transpose(Augmented(1:paramsNum))) -  coefficients(i,i).* Augmented(i);
-        Augmented(i) = (Bs(i) - Augmented(i)) / coefficients(i,i);
+        multi_sum = sum(coefficients(i,:).*transpose(Augmented(1:paramsNum))) -  coefficients(i,i).* Augmented(i);
+        Augmented(i) = relax * (Bs(i) - multi_sum) / coefficients(i,i) + Augmented(i) * (1 - relax);
     end
     
     EAcalculated = abs ((Augmented(1:paramsNum) - Augmented( paramsNum + 1 : 2*paramsNum)) ./ Augmented(1:paramsNum));  
+    %disp(EAcalculated)
     
-    if (sum(EAcalculated > (ones(paramsNum,1) .* Ea )) == 0)
+    if (sum(EAcalculated > (ones(paramsNum,1) .* (Ea / 100) )) == 0)
         X = Augmented(1: paramsNum);
+        it = iter;
+        EA = EAcalculated.*100;
         break
     end
     
